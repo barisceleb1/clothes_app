637c796d03d800930512cb1f5e98edecd44d26ed
barisceleb1 <barisclb71@gmail.com>
2023-10-03 22:08:22 +0300

    clothes_app

---
+++ lib/order/order_now_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OrderNowScreen extends StatelessWidget {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+
+
+
+    );
+  }
+}
